29025e9963da43ad1e6b0e7d0a28c4d471e543f3
Joseph Wasden <jpwasden@gmail.com>
2025-04-10 20:26:37 +0100

    Added 100% coverage for get_current_user

--- tests/routes/authenticate/test_current_user.py
+++ tests/routes/authenticate/test_current_user.py
@@ -1,10 +1,12 @@
 import uuid
 from datetime import datetime, timedelta
-from unittest.mock import AsyncMock, call, patch
+from unittest.mock import AsyncMock, MagicMock, PropertyMock, call, patch
 
 import jwt
+import pytest
 import pytz
 from db.psql.users.queries import GET_USER_FROM_ID
+from fastapi import HTTPException
 from freezegun import freeze_time
 from lwe import Public, Secret
 from models.user import User
@@ -29,16 +31,62 @@ REDIS_GETS = {
     f"{USER_ID}-public": PUBLIC.to_b64(),
     f"{USER_ID}-secret": SECRET.to_b64(),
 }
+REDIS_GETS_NO_IP = {
+    f"{USER_ID}-expiry": (TEST_TIME + timedelta(seconds=SESSION_DURATION)).isoformat(),
+    f"{USER_ID}-public": PUBLIC.to_b64(),
+    f"{USER_ID}-secret": SECRET.to_b64(),
+}
+REDIS_GETS_WRONG_IP = {
+    f"{USER_ID}-ip": "128.1.1.1",
+    f"{USER_ID}-expiry": (TEST_TIME + timedelta(seconds=SESSION_DURATION)).isoformat(),
+    f"{USER_ID}-public": PUBLIC.to_b64(),
+    f"{USER_ID}-secret": SECRET.to_b64(),
+}
+REDIS_GETS_NO_SECRET = {
+    f"{USER_ID}-ip": "127.0.0.1",
+    f"{USER_ID}-expiry": (TEST_TIME + timedelta(seconds=SESSION_DURATION)).isoformat(),
+    f"{USER_ID}-public": PUBLIC.to_b64(),
+}
+REDIS_GETS_NO_PUBLIC = {
+    f"{USER_ID}-ip": "127.0.0.1",
+    f"{USER_ID}-expiry": (TEST_TIME + timedelta(seconds=SESSION_DURATION)).isoformat(),
+    f"{USER_ID}-secret": SECRET.to_b64(),
+}
 
 
 def redis_get_mock_func(key: str) -> str | None:
     return REDIS_GETS.get(key)
 
 
+def redis_get_mock_func_no_ip(key: str) -> str | None:
+    return REDIS_GETS_NO_IP.get(key)
+
+
+def redis_get_mock_func_wrong_ip(key: str) -> str | None:
+    return REDIS_GETS_WRONG_IP.get(key)
+
+
+def redis_get_mock_func_no_secret(key: str) -> str | None:
+    return REDIS_GETS_NO_SECRET.get(key)
+
+
+def redis_get_mock_func_no_public(key: str) -> str | None:
+    return REDIS_GETS_NO_PUBLIC.get(key)
+
+
 @fixture
 def token() -> str:
     raw_token = {
         "id": USER_ID,
+        "expires": (TEST_TIME + timedelta(seconds=SESSION_DURATION)).isoformat(),
+        "duration": SESSION_DURATION,
+    }
+    return jwt.encode(raw_token, FAKE_SECRET)
+
+
+@fixture
+def token_no_id() -> str:
+    raw_token = {
         "expires": (TEST_TIME + timedelta(seconds=SESSION_DURATION)).isoformat(),
         "duration": SESSION_DURATION,
     }
@@ -81,3 +129,146 @@ async def test_get_current_user_positive(
     row_mock.assert_called_once_with(GET_USER_FROM_ID, USER_ID)
     assert get_mock.call_count == 4
     get_mock.assert_has_calls([call(key) for key in REDIS_GETS], any_order=True)
+
+
+@patch("db.psql.client.Psql.fetch_row", new_callable=AsyncMock, return_value=USER_FETCH)
+@patch("db.redis.client.Redis.get", side_effect=redis_get_mock_func)
+@mark.asyncio
+async def test_get_current_user_invalid_jwt(get_mock: AsyncMock, row_mock: AsyncMock, request_obj: Request) -> None:
+    with pytest.raises(HTTPException) as error_info:
+        await get_current_user("Invalid", request_obj)
+
+    assert error_info.value.status_code == 401
+    assert error_info.value.detail == "Invalid username or password"
+    row_mock.assert_not_called()
+    get_mock.assert_not_called()
+
+
+@patch("starlette.requests.Request.client", new_callable=lambda: PropertyMock(return_value=None))
+@patch("db.psql.client.Psql.fetch_row", new_callable=AsyncMock, return_value=USER_FETCH)
+@patch("db.redis.client.Redis.get", side_effect=redis_get_mock_func)
+@mark.asyncio
+async def test_get_current_user_no_client(
+    get_mock: AsyncMock, row_mock: AsyncMock, _: MagicMock, token: str, request_obj: Request
+) -> None:
+    with pytest.raises(HTTPException) as error_info:
+        await get_current_user(token, request_obj)
+
+    assert error_info.value.status_code == 401
+    assert error_info.value.detail == "Invalid client"
+    row_mock.assert_not_called()
+    get_mock.assert_not_called()
+
+
+@patch("db.psql.client.Psql.fetch_row", new_callable=AsyncMock, return_value=USER_FETCH)
+@patch("db.redis.client.Redis.get", new_callable=AsyncMock, return_value=None)
+@mark.asyncio
+async def test_get_current_user_no_expiry(
+    get_mock: AsyncMock, row_mock: AsyncMock, token: str, request_obj: Request
+) -> None:
+    with pytest.raises(HTTPException) as error_info:
+        await get_current_user(token, request_obj)
+
+    assert error_info.value.status_code == 401
+    assert error_info.value.detail == "Invalid username or password"
+    row_mock.assert_not_called()
+    get_mock.assert_called_once_with(f"{USER_FETCH['id']}-expiry")
+
+
+@patch("db.psql.client.Psql.fetch_row", new_callable=AsyncMock, return_value=USER_FETCH)
+@patch("db.redis.client.Redis.get", new_callable=AsyncMock, return_value=TEST_TIME.isoformat())
+@freeze_time((TEST_TIME + timedelta(seconds=1)).isoformat())
+@mark.asyncio
+async def test_get_current_user_expired_token(
+    get_mock: AsyncMock, row_mock: AsyncMock, token: str, request_obj: Request
+) -> None:
+    with pytest.raises(HTTPException) as error_info:
+        await get_current_user(token, request_obj)
+
+    assert error_info.value.status_code == 401
+    assert error_info.value.detail == "Token has expired"
+    row_mock.assert_not_called()
+    get_mock.assert_called_once_with(f"{USER_FETCH['id']}-expiry")
+
+
+@patch("db.psql.client.Psql.fetch_row", new_callable=AsyncMock, return_value=USER_FETCH)
+@patch("db.redis.client.Redis.get", side_effect=redis_get_mock_func_no_ip)
+@freeze_time(TEST_TIME.isoformat())
+@mark.asyncio
+async def test_get_current_user_no_ip(
+    get_mock: AsyncMock, row_mock: AsyncMock, token: str, request_obj: Request
+) -> None:
+    with pytest.raises(HTTPException) as error_info:
+        await get_current_user(token, request_obj)
+
+    assert error_info.value.status_code == 401
+    assert error_info.value.detail == "Invalid ip please reauthenticate"
+    row_mock.assert_not_called()
+    assert get_mock.call_count == 2
+    get_mock.assert_has_calls([call(f"{USER_FETCH['id']}-expiry"), call(f"{USER_FETCH['id']}-ip")])
+
+
+@patch("db.psql.client.Psql.fetch_row", new_callable=AsyncMock, return_value=USER_FETCH)
+@patch("db.redis.client.Redis.get", side_effect=redis_get_mock_func_wrong_ip)
+@freeze_time(TEST_TIME.isoformat())
+@mark.asyncio
+async def test_get_current_user_wrong_ip(
+    get_mock: AsyncMock, row_mock: AsyncMock, token: str, request_obj: Request
+) -> None:
+    with pytest.raises(HTTPException) as error_info:
+        await get_current_user(token, request_obj)
+
+    assert error_info.value.status_code == 401
+    assert error_info.value.detail == "Invalid ip please reauthenticate"
+    row_mock.assert_not_called()
+    assert get_mock.call_count == 2
+    get_mock.assert_has_calls([call(f"{USER_FETCH['id']}-expiry"), call(f"{USER_FETCH['id']}-ip")])
+
+
+@patch("db.psql.client.Psql.fetch_row", new_callable=AsyncMock, return_value=USER_FETCH)
+@patch("db.redis.client.Redis.get", side_effect=redis_get_mock_func_no_secret)
+@freeze_time(TEST_TIME.isoformat())
+@mark.asyncio
+async def test_get_current_user_no_secret(
+    get_mock: AsyncMock, row_mock: AsyncMock, token: str, request_obj: Request
+) -> None:
+    with pytest.raises(HTTPException) as error_info:
+        await get_current_user(token, request_obj)
+
+    assert error_info.value.status_code == 401
+    assert error_info.value.detail == "Invalid username or password"
+    row_mock.assert_not_called()
+    assert get_mock.call_count == 4
+    get_mock.assert_has_calls([call(key) for key in REDIS_GETS], any_order=True)
+
+
+@patch("db.psql.client.Psql.fetch_row", new_callable=AsyncMock, return_value=USER_FETCH)
+@patch("db.redis.client.Redis.get", side_effect=redis_get_mock_func_no_public)
+@freeze_time(TEST_TIME.isoformat())
+@mark.asyncio
+async def test_get_current_user_no_public(
+    get_mock: AsyncMock, row_mock: AsyncMock, token: str, request_obj: Request
+) -> None:
+    with pytest.raises(HTTPException) as error_info:
+        await get_current_user(token, request_obj)
+
+    assert error_info.value.status_code == 401
+    assert error_info.value.detail == "Invalid username or password"
+    row_mock.assert_not_called()
+    assert get_mock.call_count == 4
+    get_mock.assert_has_calls([call(key) for key in REDIS_GETS], any_order=True)
+
+
+@patch("db.psql.client.Psql.fetch_row", new_callable=AsyncMock, return_value=USER_FETCH)
+@patch("db.redis.client.Redis.get", side_effect=redis_get_mock_func)
+@mark.asyncio
+async def test_get_current_user_no_user_id(
+    get_mock: AsyncMock, row_mock: AsyncMock, token_no_id: str, request_obj: Request
+) -> None:
+    with pytest.raises(HTTPException) as error_info:
+        await get_current_user(token_no_id, request_obj)
+
+    assert error_info.value.status_code == 401
+    assert error_info.value.detail == "Invalid username or password"
+    row_mock.assert_not_called()
+    get_mock.assert_not_called()
